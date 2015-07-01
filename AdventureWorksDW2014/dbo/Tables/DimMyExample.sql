73cf1a1fcf101aee7ba99099e78f2c1c41a2ecda
JohnTunnicliffe <john@bovi.co.uk>
2015-07-01 23:41:50 +0100

    Initial code for Codeplex

---
+++ AdventureWorksDW2014/dbo/Tables/DimMyExample.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DimMyExample]
+(
+    [CustomerKey]           INT            NOT NULL,
+    [GeographyKey]          INT            NOT NULL,
+    [Title]                 NVARCHAR (8)   NULL,
+    [FirstName]             NVARCHAR (50)  NULL,
+    [Surname]				NVARCHAR (50)  NULL,
+	CONSTRAINT [PK_DimMyExample_CustomerKey_GeographyKey] PRIMARY KEY CLUSTERED ([CustomerKey] ASC, [GeographyKey] ASC),
+)
